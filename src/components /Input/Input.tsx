844026f65fdc591b69d6000d50a2d97002132428
Maria Petersen <mau.petersen@gmail.com>
2023-05-08 12:18:24 +0200

    first commit

---
+++ src/components /Input/Input.tsx
@@ -0,0 +1,5 @@
+function Input (props: {placeholder: string}) {
+    return <div><input className="input" type="text" placeholder={props.placeholder} value=""></input>
+</div>}
+
+export default Input 
